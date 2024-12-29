4ca7731a70282579fe7739b4c153db4cd8d0ed49
Ville Talonpoika <ville@talonpoika.me>
2024-12-30 01:24:37 +0200

    PWA experiment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import mdx from "@astrojs/mdx";
 import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
 import rehypeExternalLinks from "rehype-external-links";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://talonpoika.me",
-	integrations: [mdx(), svelte()],
+	integrations: [AstroPWA(), mdx(), svelte()],
 	markdown: {
 		shikiConfig: {
 			theme: "rose-pine",
