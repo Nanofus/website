09503f6fa1630aedf8ce689ee9c613ffc697f66e
Ville Talonpoika <ville@talonpoika.me>
2024-12-30 01:57:49 +0200

    PWA test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,32 @@ import mdx from "@astrojs/mdx";
 import remarkGfm from "remark-gfm";
 import remarkSmartypants from "remark-smartypants";
 import rehypeExternalLinks from "rehype-external-links";
-import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://talonpoika.me",
-	integrations: [AstroPWA(), mdx(), svelte()],
+	integrations: [mdx(), svelte()],
+	vite: {
+		manifest: {
+			name: "Ville Talonpoika",
+			short_name: "Ville Talonpoika",
+			description: "Web & game developer, working as a consultant.",
+			theme_color: "#FF6A34",
+			display: "standalone",
+			icons: [
+				{
+					src: "pwa-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "pwa-512x512.png",
+					sizes: "256x256",
+					type: "image/png",
+				},
+			],
+		},
+	},
 	markdown: {
 		shikiConfig: {
 			theme: "kanagawa-dragon",
